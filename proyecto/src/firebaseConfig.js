dd0bb7d37f55123914bb3f8c6da3c196f7bc9a08
Odinlowe1651 <odinlowe1651@gmail.com>
2025-05-28 02:39:36 +0000

    ch2

---
+++ proyecto/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkKgrkTTIVLiEt5EsM1jbDgCJKQ7ohUx8",
+  authDomain: "myapp-8d34d.firebaseapp.com",
+  projectId: "myapp-8d34d",
+  storageBucket: "myapp-8d34d.firebasestorage.appp",
+  messagingSenderId: "384143776546",
+  appId: "1:384143776546:web:23696761661db409a5a37b"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app); // ✅ ¡Esto es necesario!
+
+export { auth, db };
